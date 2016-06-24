a0861cc207927be354d6e20ec58b22541efff0f6
Ishaan Gupta <ishaan80@gmail.com>
2016-06-24 19:15:22 +0800

    Update node.js

--- node.js
+++ node.js
@@ -1,20 +1,3 @@
-/*jslint node:true, vars:true, bitwise:true, unparam:true */
-/*jshint unused:true */
-
-/*
-A simple node.js application intended to read data from Analog pins on the Intel based development boards such as the Intel(R) Galileo and Edison with Arduino breakout board.
-
-MRAA - Low Level Skeleton Library for Communication on GNU/Linux platforms
-Library in C/C++ to interface with Galileo & other Intel platforms, in a structured and sane API with port nanmes/numbering that match boards & with bindings to javascript & python.
-
-Steps for installing MRAA & UPM Library on Intel IoT Platform with IoTDevKit Linux* image
-Using a ssh client: 
-1. echo "src maa-upm http://iotdk.intel.com/repos/1.1/intelgalactic" > /etc/opkg/intel-iotdk.conf
-2. opkg update
-3. opkg upgrade
-
-Article: https://software.intel.com/en-us/html5/articles/intel-xdk-iot-edition-nodejs-templates
-*/
 
 var mraa = require('mraa'); //require mraa
 console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
@@ -29,13 +12,8 @@ outpin.write(1);
 
 analogValue = analogPin0.read(); //read the value of the analog pin
 console.log("Analog should be 1, but val is : " + analogValue); //write the value of the analog pin to the console
-
-
-
 outpin.write(0);
 
 analogValue = analogPin0.read(); //read the value of the analog pin
 console.log("Analog should be 0, but val is : " + analogValue); //write the value of the analog pin to the console
-
-
 }
